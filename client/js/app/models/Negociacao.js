136d30d00ff574ddb23d451202bcd22e8d4eec0a
Izabella Simonetti <izabellasimonetti@Izabellas-MacBook-Pro.local>
2019-12-09 09:52:44 -0300

    Creating the necogiation controller

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,15 +2,30 @@
 class Negociacao {
 
     constructor( data, quantidade, valor ) {
-        this.data = new Date();
-        this.quantidade = quantidade;
-        this.valor = valor;
+        // The underline before the properties is a convention that just the same class mothods 
+        // can access thoses no one from outside can.
+        this._data = new Date(data.getTime()); 
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
     }
     // If I am creating a function inside of a class I called that a method 
     // And if I am creating a function outside of a class I called that a function
 
-    onterVolume() {
-        return this.quantidade * this.valor;
+    get volume() { 
+        return this._quantidade * this._valor;
     }
+    // The methods of the same class can access the properties 
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+
 }
 
